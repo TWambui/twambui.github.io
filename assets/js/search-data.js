415987d14d8d82271aacaa396008888842161022
TWambui <92646873+TWambui@users.noreply.github.com>
2025-11-07 21:23:37 +0000

    Deploying to gh-pages from @ TWambui/TWambui.github.io@dd1e0f61e3aeb5c50cffeca0e22d99b19e26a2bf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,16 +391,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-paper-accepted-for-the-trb-annual-meeting-2026-happy-to-chat-during-the-conference",
+          title: 'Paper accepted for the TRB Annual Meeting 2026! Happy to chat during the...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
